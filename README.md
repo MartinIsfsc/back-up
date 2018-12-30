2a9356b46a3108618a31eee9aa51b88d7248ca04
MartinIsfsc <44038034+MartinIsfsc@users.noreply.github.com>
2018-12-30 20:11:12 +0100

    Un petit back-up avant le php et le bootstrap

---
+++ README.md
@@ -0,0 +1,5 @@
+# The-Layers-Project
+The building of a brand new city
+
+
+We are a team of two young students, who have as project this year to build a site that demonstrates their idea of a city leveled to optimize mobility !
---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  
+  <head>
+    <meta charset="utf-8">
+    <title>The Layers Project</title>
+    <link rel="icon" type="image/png" href="/image/xi.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="custom.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+  
+
+  </head>
+  
+  <body>
+
+    
+    <!-- LE MENU -->
+    
+    <!-- <header>
+
+      <a href="index.html" id="menu_logo"><img src="image/tlp_blanc.png" alt="THE LAYERS PROJECT" id="menu_logo_link"></a>
+      <span id="menu_accueil"> <a href="index.html" class="menu_link">accueil</a> </span>
+      <span id="menu_projet"> <a href="projet.html" class="menu_link"> projet </a></span>
+      <span id="menu_asbl"> <a href="asbl.html" class="menu_link"> asbl</a></span>
+      <span id="menu_agenda"> <a href="agenda.html" class="menu_link"> agenda</a> </span>
+      <span id="menu_news"><a href="news.html" class="menu_link">news</a> </span>
+      <span id="menu_galerie"><a href="galerie.html" class="menu_link">galerie</a> </span>
+
+    </header> -->
+
+       <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top ">
+              <a href="index.html" class="navbar-brand"><img src="image/tlp_blanc.png" alt="THE LAYERS PROJECT" class="d-inline-block align-top menu-logo-link"> </a>
+
+                     <ul class="navbar-nav flex-space-between">
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link"> accueil</a>
+                            </li>
+
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link">projet</a>
+                            </li>
+
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link">asbl</a>
+                            </li>
+                            
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link">agenda</a>
+                            </li>
+                            
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link">news</a>
+                            </li>
+                            
+                            <li class="nav-item">
+                                   <a href="#" class="nav-link menu_link">galerie</a>
+                            </li>
+                     </ul>
+       </nav>
+
+    
+    
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  </body>
