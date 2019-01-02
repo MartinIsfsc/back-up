eb391156ddd76979683db1b11f9c0e455b986a0f
MartinIsfsc <44038034+MartinIsfsc@users.noreply.github.com>
2019-01-02 02:45:13 +0100

    Que se passe-t-il ?

---
+++ footer.php
@@ -0,0 +1,76 @@
+<footer>
+      
+      <span id="footer_coordonnees_titre">coordonnées</span>
+                 
+      
+      <div id="footer_coordonnees_content">
+            ASBL THE LAYERS PROJECT <br>
+            Rue des charbonniers 12 <br>
+            1020 Bruxelles <br>
+            <br>
+            TEL : <a href="callto:+3227343434" class="footer_link">+322 734 34 34</a>  <br>
+            MAIL : <a href="mailto:layers.project@gmail.com" class="footer_link"> layers.project@gmail.com </a><br>
+      </div>
+    
+
+
+      <span id="footer_map_titre"> map du site</span>
+  
+      <div id="footer_map_content">
+        
+        <p>
+            <a href="index.php" class="footer_link"> Accueil</a> <br>
+            
+            <a href="projet.php" class="footer_link">Projet</a> <br>
+            
+            <a href="asbl.php" class="footer_link">ASBL</a> <br>
+            <br>
+            <a href="agenda.php" class="footer_link">Agenda</a> <br>
+            
+            <a href="news.php" class="footer_link">News</a> <br>
+            
+        </p>
+
+      </div>
+
+      
+      <span id="footer_asbl_titre">l'asbl</span>
+
+      <div id="footer_asbl_content">
+        
+        <p>
+          Nous sommes une jeune ASBL belge qui a pour but de promouvoir le projet d'aménagement des villes dans l'optique de l'optimisation de la circulation, la modernité, l'écologie et le confort dans les transports. 
+          <br>
+          <br>
+          Rejoignez-nous sur les réseaux : <br>
+          <a href="https://www.facebook.com/The-Layers-Project-438733686661474/?modal=admin_todo_tour"><i class="fab fa-facebook-square fa-2x"></i></a>
+          <a href="https://twitter.com"><i class="fab fa-instagram fa-2x"></i></a>  
+          <a href="https://instagram.com"><i class="fab fa-twitter-square fa-2x"></i></a>
+        </p>
+
+      </div>
+
+      
+      <span id="footer_copyright">
+          
+        <span id="footer_copyright_content">
+            COPYRIGHT 2018 | Tous droits réservés | 
+            <a href="index.php" class="footer_link">thelayersproject.be</a> | 
+            <a href="conditions.php" class="footer_link"> Conditions générales d'utilisation </a> 
+            et 
+            <a href="mentions_legales.php" class="footer_link"> Mentions Légales</a>
+      
+        </span>
+        
+      </span>  
+    
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+  </body>
+
+</html>
--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="/css/master.css">
-    <link rel="stylesheet" href="custom.css">
+    <link rel="stylesheet" href="custom-bootstrap.css">
     <link href="https://fonts.googleapis.com/css?family=Exo:400,600,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
   
@@ -68,8 +68,5 @@
     
     
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
+    
   </body>
